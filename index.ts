a18123571644e1e50e66073cf01ed060ff0399ab
Erkan Buelbuel <webia1.github@gmail.com>
2021-09-24 13:16:50 +0200

    INIT

---
+++ index.ts
@@ -0,0 +1,17 @@
+const apiResponse = [
+  {
+    firstName: "James",
+    lasName: "Bond",
+    age: 50,
+  },
+  {
+    firstName: "Michael",
+    lasName: "Jackson",
+    age: 55,
+  },
+  {
+    firstName: "Britney",
+    lasName: "Spears",
+    age: 35,
+  },
+];
