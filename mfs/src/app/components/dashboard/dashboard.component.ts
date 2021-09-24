2c782c93e22e1f8c5b48e1b13a562828d7011261
Erkan Buelbuel <webia1.github@gmail.com>
2021-09-24 14:53:55 +0200

    login page -> dashboard -> back

--- mfs/src/app/components/dashboard/dashboard.component.ts
+++ mfs/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout() {
+    this.router.navigate(['']);
   }
-
 }
